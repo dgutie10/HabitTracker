d4927958bcfd61aca2628ab4cef38b5f18994c09
dgutie10 <dgutie10@gmail.com>
2016-12-02 17:01:21 -0500

    Done for the day

--- app/src/main/java/com/example/android/habittracker/data/HabitContract.java
+++ app/src/main/java/com/example/android/habittracker/data/HabitContract.java
@@ -1,8 +1,19 @@
 package com.example.android.habittracker.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by diegog on 12/2/2016.
  */
 
 public class HabitContract  {
+    private HabitContract(){}
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.habittracker";
+
+    public static final class ActivityEntry implements BaseColumns{
+        public static final String TABLE_NAME = "activity";
+        
+    }
+
 }
